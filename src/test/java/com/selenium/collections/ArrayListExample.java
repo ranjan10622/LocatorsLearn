69422b621e1fd2c18c95f8b5e6a7e96c842bc6ae
Ranjan <ranjan22mohan@gmail.com>
2024-02-18 11:38:27 +0530

    collection concepts learning

---
+++ src/test/java/com/selenium/collections/ArrayListExample.java
@@ -0,0 +1,37 @@
+package com.selenium.collections;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		ArrayList<String> str = new ArrayList<String>();
+		  str.add("Ranjan");
+		  str.add("Java");
+		  str.add(1, "Mohan");
+		  
+		  System.out.println(str);
+		  
+		  
+		/*  str.remove(1);
+		  
+		  System.out.println(str);
+		 str.remove("Java");
+		  
+		  System.out.println(str);
+		  
+		  str.removeAll(str);
+		  
+		  System.out.println(str);
+		  
+		  */
+		System.out.println(str.get(2));
+		
+		System.out.println(str.contains("baba"));
+
+	}
+
+}
